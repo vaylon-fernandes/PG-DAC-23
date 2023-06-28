e94db2b8d9812f878ba3895b7148bf3a865221ca
vaylon-fernandes <vaylonfernandes@gmail.com>
2023-06-29 01:52:37 +0530

    Minor changes

--- Advanced Java/project_mgmt_spring/src/main/java/com/app/controller/ProductController.java
+++ Advanced Java/project_mgmt_spring/src/main/java/com/app/controller/ProductController.java
@@ -3,13 +3,12 @@ package com.app.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.pojos.Product;
@@ -33,7 +32,7 @@ public class ProductController {
 		return productService.addProduct(product);
 	}
 	@GetMapping("/{id}")
-	public String getById(@PathVariable Long id){
+	public @ResponseBody String getById(@PathVariable Long id){
 		String mesg = "";
 		System.out.println("In get by Id");
 		Product prod =null;
