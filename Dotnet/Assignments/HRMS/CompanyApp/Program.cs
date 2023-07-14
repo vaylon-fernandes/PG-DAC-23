d1759c88809166c2faa1b2ed50cf0a44cf0b7808
vaylon-fernandes <vaylonfernandes@gmail.com>
2023-07-15 02:05:51 +0530

    added more functionality to hrms assignment

--- Dotnet/Assignments/HRMS/CompanyApp/Program.cs
+++ Dotnet/Assignments/HRMS/CompanyApp/Program.cs
@@ -3,28 +3,17 @@ using System.Linq.Expressions;
 using Taxation;
 using static Taxation.Dispatcher;
 
-/*HR newEmp = new HR(101, "Jonh", "King", "HR", 10000,100);
-//newEmp.computeSalary();
-List<HR> list = new List<HR>();
-list.Add(newEmp);
 
-TaxManager taxManager = new TaxManager();
-
-TaxOperation operation1 = new TaxOperation(taxManager.PayIncomeTax);
-double sal = operation1(newEmp.salary);
-newEmp.salary = sal;
-newEmp.computeSalary();
-
-Console.WriteLine(newEmp.salary);*/
 List<Employee> list = new List<Employee>();
+Employee newEmployee = null;
 Boolean flag =false;
 while (!flag)
 {
     Console.WriteLine("Enter Your choice: ");
-    Console.WriteLine("1. Hire Sales Manager\n2. Show all employees:\n3. Show employee details:\n4.Remove Employee:\n5.Exit");
+    Console.WriteLine("1. Hire Employee\n2. Show all employees:\n3. Show employee details:\n4.Remove Employee:\n5. Give Bonus\n6.Exit");
     int choice = int.Parse(Console.ReadLine());
     switch (choice){
-        case 1: // hire Sales Manager 
+        case 1: // hire Employee
             Console.WriteLine("Enter employee details:");
             Console.WriteLine("Enter Employee ID:");
             int empId = int.Parse(Console.ReadLine());
@@ -36,7 +25,17 @@ while (!flag)
             string department = Console.ReadLine();
             Console.WriteLine("Enter Salary:");
             double salary = double.Parse(Console.ReadLine());
-            Employee newEmployee = new SalesManager(empId, empName,empLastName, department, salary);
+            Console.WriteLine("Choose Role:\n1.Sales Manager\n2.HR Manager");
+            int roleChoice = int.Parse(Console.ReadLine());
+            switch (roleChoice)
+            {
+                case 1:
+                    newEmployee = new SalesManager(empId, empName, empLastName, department, salary);
+                    break;
+                case 2:
+                     newEmployee = new HRManager(empId, empName, empLastName, department, salary);
+                    break;
+            }
             list.Add(newEmployee);
             Console.WriteLine("Employee added successfully");
             break;
@@ -82,7 +81,32 @@ while (!flag)
             }
 
             break;
-        case 5:
+        case 5: // assgin bonus
+            Console.WriteLine("Enter Employee Id: ");
+            int empIdToGiveBonus = int.Parse(Console.ReadLine());
+             result= list.Find(emp => emp.empId == empIdToGiveBonus);
+            if (result != null)
+            {
+                Console.WriteLine(result);
+                Console.WriteLine("Enter Bonus amount: ");
+                int bonus = int.Parse(Console.ReadLine());
+                if(result is SalesManager)
+                {
+                    SalesManager salesManager = (SalesManager)result;
+                    salesManager.perfBonus = bonus;
+                }                
+                else if(result is HRManager){
+                    HRManager hrManager = (HRManager)result;
+                    hrManager.perfBonus = bonus;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Employee Not Found!!");
+            }
+
+            break;
+        case 6:
             Console.WriteLine("Exiting");
             flag = true;
             break;
